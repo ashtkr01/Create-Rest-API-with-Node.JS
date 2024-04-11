920d2843215f900e62e76eb0dc95f1b38eb5f047
ashtkr01 <ashishkumarkatheriyaa@gmail.com>
2024-04-11 10:37:45 +0530

    create Server with Express JS

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req , res) => {
+    res.send("Hi, I am Live");
+});
+
+const start = async()=>{
+    try {
+        app.listen(PORT, () => {
+            console.log(`${PORT} Yes I am connected`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+//Call:
+start();
